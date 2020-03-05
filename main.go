fb9d963db7297fa0ee5dcafb774eb68864f5d050
kenshiro41 <ken0401shiro@gmail.com>
2020-03-05 23:35:07 +0900

    first commit

---
+++ main.go
@@ -0,0 +1,99 @@
+package main
+
+import (
+	"database/sql"
+	"encoding/json"
+	"io/ioutil"
+
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+	_ "github.com/lib/pq"
+)
+
+func main() {
+	r := gin.Default()
+	config := cors.DefaultConfig()
+	config.AllowOrigins = []string{"http://localhost:4001"}
+	r.Use(cors.New(config))
+	r.GET("/v1/player", player)
+	r.GET("/v1/jleague/j1/result", jleague)
+	r.GET("/v1/jleague/j1/result/:section", resultSection)
+	r.Run(":5000")
+}
+
+func player(c *gin.Context) {
+	p, err := ioutil.ReadFile("./player.json")
+	if err != nil {
+		print(err)
+	}
+	var pl PLAYERS
+	json.Unmarshal(p, &pl)
+	c.JSON(200, pl)
+}
+
+//PLAYERS DO
+type PLAYERS struct {
+	PLAYERS []DATA `json:"player"`
+}
+
+//DATA DO
+type DATA struct {
+	I    int    `json:"u"`
+	NAME string `json:"name"`
+}
+
+//RESULTS D
+type RESULTS struct {
+	RESULTS []RESULT `json:"result"`
+}
+
+//RESULT D
+type RESULT struct {
+	ID      int    `json:"id"`
+	SEC     int    `json:"section"`
+	HOME    string `json:"home"`
+	HS      int    `json:"home_score"`
+	AWAY    string `json:"away"`
+	AS      int    `json:"away_score"`
+	DATE    string `json:"date"`
+	VISITOR string `json:"visitor"`
+	KO      string `json:"ko"`
+	STADIUM string `json:"stadium"`
+}
+
+func jleague(c *gin.Context) {
+	db, err := sql.Open("postgres", "host=127.0.0.1 port=5432 user=ken41 password=kimosken41 dbname=jleague sslmode=disable")
+	if err != nil {
+		panic(err)
+	}
+	rows, err := db.Query("SELECT * FROM result.match_result")
+	if err != nil {
+		panic(err)
+	}
+	results := []RESULT{}
+	result := RESULT{}
+	for rows.Next() {
+		rows.Scan(&result.ID, &result.SEC, &result.HOME, &result.HS, &result.AWAY, &result.AS, &result.DATE, &result.VISITOR, &result.KO, &result.STADIUM)
+		results = append(results, result)
+	}
+	c.JSON(200, results)
+}
+
+func resultSection(c *gin.Context) {
+	section := c.Param("section")
+	db, err := sql.Open("postgres", "host=127.0.0.1 port=5432 user=ken41 password=kimosken41 dbname=jleague sslmode=disable")
+	if err != nil {
+		panic(err)
+	}
+	rows, err := db.Query("SELECT * FROM result.match_result where section = $1", section)
+	if err != nil {
+		panic(err)
+	}
+	results := []RESULT{}
+	result := RESULT{}
+	for rows.Next() {
+		rows.Scan(&result.ID, &result.SEC, &result.HOME, &result.HS, &result.AWAY, &result.AS, &result.DATE, &result.VISITOR, &result.KO, &result.STADIUM)
+		results = append(results, result)
+	}
+	c.JSON(200, results)
+}
---
+++ main.py
@@ -0,0 +1,71 @@
+from bs4 import BeautifulSoup
+import requests
+import json
+import lxml
+import os
+import psycopg2
+import numpy as np
+import re
+
+
+section = int
+home = str
+home_score = int
+away = str
+away_score = int
+date = str
+visitor = str
+ko = str
+stadium = str
+data = []
+
+target_url = 'https://data.j-league.or.jp/SFTP01/'
+req = requests.get(target_url)
+soup = BeautifulSoup(req.content, 'lxml')
+main_contents = soup.find('div', class_='tab-contents-main').stripped_strings
+sec = soup.find('h2', class_='tab-title').stripped_strings
+findint = int
+
+for s in sec:
+  findint = re.findall(r'\d+', s)
+
+# insert some values in array
+for index, value in enumerate(main_contents):
+  if index == 0:
+    continue
+  data.append(value)
+arr = list(np.array_split(data, 9))
+
+# inser values to postgres
+connection = psycopg2.connect(
+    'postgresql://ken41:kimosken41@127.0.0.1:5432/jleague')
+section = int(findint[2])
+for i in range(len(arr)):
+  li = arr[i]
+  home = li[0]
+  home_score = li[1]
+  away = li[4]
+  away_score = li[3]
+  date = li[6]
+  visitor = li[8]
+  ko = li[10]
+  stadium = li[12]
+  try:
+    with connection.cursor() as cur:
+      cur.execute(
+          'INSERT INTO result.match_result (section, home, home_score, away, away_score, date, visitor, ko, stadium) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)',
+          (
+              section,
+              home,
+              home_score,
+              away,
+              away_score,
+              date,
+              visitor,
+              ko,
+              stadium)
+      )
+    connection.commit()
+    print('success')
+  except psycopg2.DatabaseError as err:
+    print(err)
